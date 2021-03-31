a645a5f4ccdbd21c8be7bbde2752f4eb3cd433c8
Kozer2 <benhemann@yahoo.com>
2021-03-31 16:40:02 -0500

    added first test to test for book count in library

--- Lab08/Classes/Book.cs
+++ Lab08/Classes/Book.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Lab08_Collections.Classes
 {
-    class Book
+    public abstract class Book
     {
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        public int Pages { get; set; }
     }
 }
